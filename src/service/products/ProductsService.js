016eb591354a1d99afa75286f8583c2bd0abab56
Guilherme Hendres <ghendressmelo@gmail.com>
2021-10-25 19:24:50 -0300

    Criar diretório de services para as requisições da api

---
+++ src/service/products/ProductsService.js
@@ -0,0 +1,13 @@
+class ProductService {
+    // async index() { }
+
+    // async show(id) { }
+
+    // async store(data) { }
+
+    // async update(id, data) { }
+
+    // async destroy(id) { }
+}
+
+module.exports = new ProductService
