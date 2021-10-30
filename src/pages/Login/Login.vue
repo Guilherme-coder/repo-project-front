438a232fedda19d79fd64d4f254e8d983c0cdfe4
Guilherme Hendres <ghendressmelo@gmail.com>
2021-10-30 18:22:44 -0300

    cocnfigurando rotas e bootstrap

---
+++ src/pages/Login/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="box">
+      <h1>Faça seu login</h1>
+      <form action="login()">
+          <label class="login_label">Email: <input type="email" ref="email"></label>
+          <label class="login_label">Senha: <input type="password" ref="passwor"></label>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .login_label{
+        display: block;
+        margin-bottom: 1em;
+    }
+</style>
