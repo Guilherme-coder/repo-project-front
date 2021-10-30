01bc2f6f9b74a2eddba9a9f7b5698279ab408785
ArnoldSGR <arnoldrodrigues11@gmail.com>
2021-10-30 17:32:59 -0300

    home init
    Primeiro commit de componentes da home page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navBar />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/navBar.vue'
 import product from './service/products/ProductsService'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar
   },
   mounted() {
     product.index()
@@ -27,6 +26,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ src/components/navBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="navBody">
+      <button 
+      style="width: 5vw;
+      height: 5vw;
+      background-color: #42b983;
+      color:white;
+      border: none;
+      border-radius: 8px;">massa</button>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .navBody{
+    background-color: #121d27 ;
+    width: 10vw;
+    height: 100vh;
+  }
+</style>
